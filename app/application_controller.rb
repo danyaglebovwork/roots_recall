705ebd426527f371193a6d4338989f6dd7519740
Danil <danil.glebov@dualbootpartners.com>
2024-03-30 11:20:11 +0300

    added boostrap and personal area

--- app/application_controller.rb
+++ app/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::Base
+  protected def authenticate!
+    access_denied!(message: "Нет доступа!") unless authenticated?
+  end
+
+  protected def authenticated?
+    current_session.present?
+  end
+
+  protected def access_denied!(message: nil)
+    raise Errors::AccessDenied, message || "Нет доступа!"
+  end
+
+  protected def current_session
+    @current_session ||= self.session
+  end
+
+  protected def current_owner
+    @current_owner ||= User.find_by(id: current_session[:user_id])
+  end
 end
