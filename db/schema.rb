533b83c5ceabff2542c40c7da37fce647d86f932
Danil <danil.glebov@dualbootpartners.com>
2024-03-30 19:50:33 +0300

    added Flashbacks

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.2].define(version: 2024_03_24_044427) do
+ActiveRecord::Schema[7.2].define(version: 2024_03_29_104633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "flashbacks", force: :cascade do |t|
+    t.bigint "user_id"
+    t.string "title", null: false
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_flashbacks_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "first_name"
