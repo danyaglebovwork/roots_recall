4ba48ff2771cfdaa3e8ef959aeb37c8f0df569c3
Danil <danil.glebov@dualbootpartners.com>
2024-04-07 12:46:52 +0300

    added draft telegram:application_handler and added draft methods

--- app/application_contract.rb
+++
@@ -1,4 +0,0 @@
-class ApplicationContract < Dry::Validation::Contract
-  config.messages.backend = :i18n
-  config.messages.top_namespace = :validations
-end
--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :email, presence: true
-
-  has_secure_password :password
-end
